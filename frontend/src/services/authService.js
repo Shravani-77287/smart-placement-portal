45f564835d596f0463d7142619c7d1d02efc6ac1
shravani <shravani77287@gmail.com>
2026-05-28 15:51:22 +0530

    Update API URLs for production

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 // src/services/authService.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth/';
+const API_URL = 'https://smart-placement-portal-gren.onrender.com/api/users/';
 
 // Register user
 const register = async (userData) => {
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/users/';
+const API_URL = 'https://smart-placement-portal-gren.onrender.com/api/users/';
 
 const uploadResume = async (formData) => {
   const user = JSON.parse(localStorage.getItem('user'));
