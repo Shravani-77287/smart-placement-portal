a178c86a4f85c09503cdf45974e4eed7ec822747
shravani <shravani77287@gmail.com>
2026-05-28 16:20:55 +0530

    Fix auth API route URL

--- frontend/src/services/quizService.js
+++ frontend/src/services/quizService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://smart-placement-portal-gren.onrender.com/api/users/';
+const API_URL = 'https://smart-placement-portal-gren.onrender.com/api/quiz/';
 
 const getAuthHeaders = () => {
   const user = JSON.parse(localStorage.getItem('user'));
